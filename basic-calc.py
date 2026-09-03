8f9448eac9eccd13fb12185259f76c0d25099484
Franklin Kreleger <Franklinkreleger@gmail.com>
2026-09-03 14:41:15 +0200

    aded user_input and select_operation function

--- basic-calc.py
+++ basic-calc.py
@@ -9,3 +9,21 @@ def multiply(x,y):
 
 def divide(x,y):
     return x/y
+
+def user_input():
+    # ask user input
+    # checks if it is a number
+    pass
+
+def select_operation():
+    pass
+
+'''
+Weclome to the calculator!
+Press for operation:
+1: Add
+2: Substract
+3: Multiply
+4: Divide
+5: Exit
+'''
